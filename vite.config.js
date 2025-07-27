656f0a32ed5325b845a186e69c234df073493320
Nsk Abdul <nsk.abdul786@gmail.com>
2025-07-27 16:46:11 +0530

    Final version

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,14 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import path from 'path'; // Add this line
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
+                'resources/scss/app.scss',
                 'resources/js/app.js',
-                'resources/scss/app.scss', // Add this line
             ],
             refresh: true,
         }),
     ],
-    resolve: { // Add this whole block
-        alias: {
-            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-        }
-    },
 });
